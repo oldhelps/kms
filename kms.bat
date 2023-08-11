47de5f1d786b0b8db478876cd3322c038ab93655
Oldhelps <99809158+oldhelps@users.noreply.github.com>
2023-08-12 07:02:05 +0800

    Add files via upload

---
+++ kms.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ==============================================
+echo OLDHELPS KMS ACTIVETOR VERSION 2.0 GNU License
+echo If there is a pop-up window, click OK.
+echo http://oldhelps.html-5.me 
+echo ==============================================
+
+slmgr/skms kms.03k.org
+slmgr/ato
